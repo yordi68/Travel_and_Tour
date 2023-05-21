c8325df69f850a4094c26c9e5a89772d3185a81a
@yordi68 <yordanos.yirgu1@gmail.com>
2023-05-21 15:30:52 +0300

    Initial commit

---
+++ about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+
+    <!-- swiper cdn link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+
+    <!-- font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+    
+<!-- header section start -->
+<section class="header">
+    <a href="home.php" class="logo"> Travel</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+
+
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+
+<div class="heading" style="background:url(images/header-bg-1.jpg) no repeat">
+    <h1>about us</h1>
+</div>
+
+
+
+
+<!-- footer section starts -->
+    <section class="footer">
+        <div class="box-container">
+        <div class="box">
+
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>  package</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>  book</a>
+
+        </div>
+
+
+            <div class="box">
+
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+
+            </div>
+
+
+            <div class="box">
+
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-angle-phone"></i> +251-0956348779</a>
+                <a href="#"> <i class="fas fa-angle-phone"></i> +251-0956348779</a>
+                <a href="#"> <i class="fas fa-angle-envelope"></i> yodanos.yirgu@gmail.com</a>
+                <a href="#"> <i class="fas fa-angle-map"></i> Ethiopia, AddisAbaba - 1000</a>
+
+            </div>
+
+
+            <div class="box">
+
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+
+            </div>
+
+
+
+        </div>
+
+
+        <div class="credit">created by <span>Yordanos Yirgu </span> | all rights reserved! </div>
+    </section>
+<!-- footer section ends  -->
+
+
+
+<!--  swiper js link--> 
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+</body>
+</html>
